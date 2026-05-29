f158d817a0ccd9f17bf4189e3cbb6253bc4f82f9
Gabriel Gramacho <ggramacho09@gmail.com>
2026-05-29 16:51:44 -0300

    fix: use leading slash for STATIC_URL to generate absolute static paths

--- atlas_vivo/settings.py
+++ atlas_vivo/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Use leading slash so `{% static %}` generates absolute paths like `/static/...`
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Use WhiteNoise static files storage for efficient serving
